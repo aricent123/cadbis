c118784f6400b31b35e7a56126fb00e31c6012af
smecsia <smecsia@254517df-764a-0410-b743-7361b4a10c92>
2008-04-14 17:11:30 +0000

    Initial import.

---
+++ trunk/CADBiS_proxy/src/cadbis/proxy/ClientToProxyIoHandler.java
@@ -0,0 +1,73 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *
+ */
+package cadbis.proxy;
+
+import java.net.SocketAddress;
+
+import org.apache.mina.common.ConnectFuture;
+import org.apache.mina.common.IoConnector;
+import org.apache.mina.common.IoFutureListener;
+import org.apache.mina.common.IoSession;
+import org.apache.mina.common.RuntimeIoException;
+import org.apache.mina.common.TrafficMask;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Handles the client to proxy part of the proxied connection.
+ *
+ * @author The Apache MINA Project (dev@mina.apache.org)
+ * @version $Rev$, $Date$
+ *
+ */
+public class ClientToProxyIoHandler extends AbstractProxyIoHandler {
+    private final ServerToProxyIoHandler connectorHandler = new ServerToProxyIoHandler();
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+    private final IoConnector connector;
+
+    private final SocketAddress remoteAddress;
+
+    public ClientToProxyIoHandler(IoConnector connector,
+            SocketAddress remoteAddress) {
+        this.connector = connector;
+        this.remoteAddress = remoteAddress;
+        connector.setHandler(connectorHandler);
+    }
+
+    @Override
+    public void sessionOpened(final IoSession session) throws Exception {
+
+        connector.connect(remoteAddress).addListener(new IoFutureListener<ConnectFuture>() {
+            public void operationComplete(ConnectFuture future) {
+                try {
+                    future.getSession().setAttribute(session);
+                    session.setAttribute(future.getSession());
+                    future.getSession().setTrafficMask(TrafficMask.ALL);
+                } catch (RuntimeIoException e) {
+                    // Connect failed
+                    session.close();
+                } finally {
+                    session.setTrafficMask(TrafficMask.ALL);
+                }
+            }
+        });
+    }
+}
