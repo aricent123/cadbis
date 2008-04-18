768b6e7d2841159b524a97bb3a5dfab15a783849
smecsia <smecsia@254517df-764a-0410-b743-7361b4a10c92>
2008-04-18 23:03:54 +0000

    Initial import.

---
+++ CADBiS_categorizer/src/cadbis/proxy/categorizer/Categorizer.java
@@ -0,0 +1,13 @@
+package cadbis.proxy.categorizer;
+
+public class Categorizer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
