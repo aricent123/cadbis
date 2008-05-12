d0d11e21dd55e6379d0b7f2a30f379795bb87e1e
drdrypa <drdrypa@254517df-764a-0410-b743-7361b4a10c92>
2008-05-12 19:36:41 +0000

    topurl

--- trunk/CADBiS_website/test/index.php
+++ trunk/CADBiS_website/test/index.php
@@ -5,7 +5,8 @@
 //include charts.php to access the SendChartData function
 include "charts.php";
 
-echo InsertChart ( "charts.swf", "charts_library", "data.php?chart_type=loading",600, 400 );
+//echo InsertChart ( "charts.swf", "charts_library", "data.php?chart_type=loading",600, 400 );//нагрузка на канал
+echo InsertChart ( "charts.swf", "charts_library", "data.php?chart_type=topurl&uid=0&gid=1",800, 400 );//topurl
 ?>
 
 </body></html>
