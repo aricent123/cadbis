ba03662613fc49b7ef1bc24a3bd596ad325b40bb
drdrypa <drdrypa@254517df-764a-0410-b743-7361b4a10c92>
2008-05-13 18:15:54 +0000

    позырь плиз, никак не могу допереть путь к chart_data относительно chart.swf

--- trunk/CADBiS_website/test/test_chart.php
+++ trunk/CADBiS_website/test/test_chart.php
@@ -3,10 +3,10 @@
 <?php
 
 //include charts.php to access the SendChartData function
-include "charts.php";
+include "../skins/smadbis/billing/cadbisnew/graph/charts.php";
 
 //echo InsertChart ( "charts.swf", "charts_library", "data.php?chart_type=loading",600, 400 );//нагрузка на канал
-echo InsertChart ( "charts.swf", "charts_library", "data.php?chart_type=topurl&uid=0&gid=1&limit=50",800, 400 );//topurl
+echo InsertChart ( "../skins/smadbis/billing/cadbisnew/graph/charts.swf", "charts_library", "../../chart_data.php?chart_type=topurl&uid=16&gid=6&limit=50",800, 400 );//topurl
 ?>
 
 </body></html>
