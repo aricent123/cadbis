92bb7c422288460c77b99fccde7db4db3f4a88f0
drdrypa <drdrypa@254517df-764a-0410-b743-7361b4a10c92>
2008-05-15 08:30:11 +0000

    для теста2 надо определиться с расположением файлов и с подключением к БД.

--- trunk/CADBiS_website/test/test_chart.php
+++ trunk/CADBiS_website/test/test_chart.php
@@ -5,8 +5,8 @@
 //include charts.php to access the SendChartData function
 include "../skins/smadbis/billing/cadbisnew/graph/charts.php";
 
-//echo InsertChart ( "../skins/smadbis/billing/cadbisnew/graph/charts.swf", "charts_library", "data.php?chart_type=loading",600, 400 );//нагрузка на канал
-//echo InsertChart ( "../skins/smadbis/billing/cadbisnew/graph/charts.swf", "charts_library", "../skins/smadbis/billing/chart_data.php?chart_type=topurl&uid=16&gid=6&limit=50",800, 400 );//topurl
+echo InsertChart ( "../skins/smadbis/billing/cadbisnew/graph/charts.swf", "../skins/smadbis/billing/cadbisnew/graph/charts_library", "../skins/smadbis/billing/chart_data.php?chart_type=loading",600, 400 );//нагрузка на канал
+echo InsertChart ( "../skins/smadbis/billing/cadbisnew/graph/charts.swf", "../skins/smadbis/billing/cadbisnew/graph/charts_library", "../skins/smadbis/billing/chart_data.php?chart_type=topurl&uid=16&gid=6&limit=50",800, 400 );//topurl
 echo InsertChart ( "../skins/smadbis/billing/cadbisnew/graph/charts.swf", "../skins/smadbis/billing/cadbisnew/graph/charts_library", "../skins/smadbis/billing/chart_data.php?chart_type=today&uid=16&gid=6&limit=50",800, 400 );//topurl
 ?>
 
